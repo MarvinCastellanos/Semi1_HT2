fa5bc18f1a196ac96167436cceb06b776b7c9934
Marvin <madacast2@gmail.com>
2026-09-06 19:31:00 -0600

    Apps

---
+++ HT2/api.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/check', (req, res) => {
+    res.status(200).send();
+});
+
+app.get('/info', (req, res) => {
+    res.json({
+        "Instancia": "Maquina 2 - Api Node",
+        "Curso": "Seminario de Sistemas 1 A",
+        "Grupo": "Grupo 1"
+    });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`API corriendo en el puerto ${PORT}`);
+});
---
+++ HT2/api.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/check', methods=['GET'])
+def check():
+    return "", 200
+
+@app.route('/info', methods=['GET'])
+def info():
+    return jsonify({
+        "Instancia": "Maquina 1 - Api Python",
+        "Curso": "Seminario de Sistemas 1 A",
+        "Grupo": "Grupo 1"
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
